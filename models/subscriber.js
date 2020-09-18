263f5c3fe49192ff3aefccbdeb36c5cdac76af9c
unknown <ajmeramanit230734@gmail.com>
2020-09-18 18:21:25 +0530

    third commit

--- models/subscriber.js
+++ models/subscriber.js
@@ -1,1 +1,19 @@
 const mongoose = require('mongoose')
+
+const subsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+      },
+      subedToChannel: {
+        type: String,
+        required: true
+      },
+      subDate: {
+        type: Date,
+        required: true,
+        default: Date.now
+      }
+})
+
+module.exports = mongoose.model('Subscriber', subsSchema)
--- routes/subscibers.js
+++ routes/subscibers.js
@@ -1,25 +1,53 @@
-const { Router } = require('express')
+const Sub = require('../models/subscriber')
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Hello World')
+router.get('/', async (req, res) => {
+    try {
+        const subscribers = await Sub.find()
+        res.json(subscribers)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 })
 
 router.get('/:id', (req, res) => {
     req.params.id
 })
 
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+    const subscriber = new Sub({
+        name: req.body.name,
+        subedToChannel: req.body.subToChannel
+    })
+    try {
+        newSub = await subscriber.save()
+        res.status(201).json(newSub)
+    } catch (error) {
+        res.status(400).json({ message: error })
+    }
 })
 
-router.patch('/' , (req, res) => {
+router.patch('/:id' , (req, res) => {
 
 })
 
 router.delete('/:id' , (req, res) => {
-
+    
 })
+
+async function getSub(req, res, next) {
+    let sub
+    try {
+        sub = await Sub.findById(req.params.id)
+        if (sub == null) {
+            return res.status(404).json({ message: 'Cannot find subscriber' })
+        }
+    } catch (err) {
+        return res.status(500).json({ messgae: err.message }) 
+    }
+
+    res.sub = sub
+}
 
 module.exports = router
