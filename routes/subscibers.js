79d6e0e51fbf0f8270a3002af946fb5c54668a21
unknown <ajmeramanit230734@gmail.com>
2020-09-19 07:56:00 +0530

    Final commit, I promise

--- routes/subscibers.js
+++ routes/subscibers.js
@@ -13,5 +13,58 @@ router.get('/', async (req, res) => {
 })
 
 
+router.post('/', async (req, res) => {
+    const subscriber = new Sub({
+        name: req.body.name,
+        subedToChannel: req.body.subToChannel
+    })
+    try {
+        newSub = await subscriber.save()
+        res.status(201).json(newSub)
+    } catch (error) {
+        res.status(400).json({ message: error })
+    }
+})
+
+router.patch('/:id', getSub, async (req, res) => {
+    if (req.body.name == null) {
+        res.sub.name = req.body.name
+    }
+
+    if (req.body.subedToChannel == null) {
+        res.sub.subedToChannel = req.body.subedToChannel
+    }
+
+    try {
+        const updatedSub = await res.sub.save()
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
+
+router.delete('/:id', getSub, async (req, res) => {
+    try {
+        await res.sub.remove()
+        res.json({ messgae: 'deleted Subscriber' })
+    } catch (err) {
+        res.status(500).json({ messgae: err.message })
+
+    }
+})
+
+async function getSub(req, res, next) {
+    let sub
+    try {
+        sub = await Sub.findById(req.params.id)
+        if (sub == null) {
+            return res.status(404).json({ message: 'Cannot find subscriber' })
+        }
+    } catch (err) {
+        return res.status(500).json({ messgae: err.message })
+    }
+
+    res.sub = sub
+    next()
+}
 
 module.exports = router
